cca402e283c8f3958ce35b4f81d5c25df8eabba5
Diego Uribe <diego.uribe@berkeley.edu>
2019-08-06 20:22:45 -0700

    implemented counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold"
+  handleIncrement = product => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </div>
     );
   }
